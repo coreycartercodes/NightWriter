dbe1fbc50247fa6ef04fd5abdb3cc480a0efd5c4
Corey Carter <crc2231@gmail.com>
2020-08-01 22:10:22 -0600

    Fix name of FileManager

--- lib/nightwriter.rb
+++ lib/nightwriter.rb
@@ -2,5 +2,5 @@ require './lib/reader'
 
 input = ARGV[0].chomp
 output = ARGV[1].chomp
-read_file = Reader.new(input, output)
-p read_file.output_message
+message = FileManager.new(input, output)
+p message.output_message
