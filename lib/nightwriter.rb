6a35e2bf261cd9c69667796d42cb671f58aada7b
Corey Carter <crc2231@gmail.com>
2020-08-02 01:20:01 -0600

    Change 'reader' to 'file_manager'

--- lib/nightwriter.rb
+++ lib/nightwriter.rb
@@ -1,6 +1,7 @@
-require './lib/reader'
+require './lib/file_manager'
 
 input = ARGV[0].chomp
 output = ARGV[1].chomp
 message = FileManager.new(input, output)
 p message.output_message
+translator = TextToBraille.new
