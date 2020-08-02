b17d75cdcdf685d8c2a10f38a90643303fac20b1
Corey Carter <crc2231@gmail.com>
2020-08-02 14:49:14 -0600

    Create nightreader and specific functions in file_manager

--- lib/file_manager.rb
+++ lib/file_manager.rb
@@ -9,12 +9,17 @@ class FileManager
     @writer = Writer.new
   end
 
-  def output_message
-    output_characers = @file_in.gsub(/\s+/, '').length
-    "Created '#{@output}' containing #{output_characers} characters"
+  def output_message_text_in
+    output_characters = @file_in.gsub(/\s+/, '').length
+    "Created '#{@output}' containing #{output_characters} characters"
   end
 
-  def create_lines
+  def output_message_braille_in
+    output_characters = (@file_in.length / 6)
+    "Created '#{@output}' containing #{output_characters} characters"
+  end
+
+  def create_lines_of_text ###does this forget the end of the message?
     text = @file_in
     lines = []
     while text.chars.length >= 40
@@ -22,17 +27,37 @@ class FileManager
       text = new_line.pop
       lines << new_line
     end
+    lines << text.split("",41)
     lines
   end
 
   def write_braille_to_file
     output_file_data = ""
-    create_lines.each do |line|
+    create_lines_of_text.each do |line|
       output_file_data << @writer.write_braille_line(line)
     end
     File.write("./data/#{@output}", output_file_data)
   end
 
+  def create_lines_of_braille
+    text = @file_in
+    lines = []
+    while text.chars.length >= 80
+      new_line = text.split("\n", 2)
+      text = new_line.pop
+      lines << new_line
+    end
+    lines << text
+    lines
+  end
+
+  def write_text_to_file
+    output_file_data = ""
+    create_lines_of_braille.each do |line|
+      output_file_data << @writer.write_text_line(line)
+    end
+    File.write("./data/#{@output}", output_file_data)
+  end
 
 
 
--- lib/nightreader.rb
+++ lib/nightreader.rb
@@ -3,5 +3,5 @@ require './lib/file_manager'
 input = ARGV[0].chomp
 output = ARGV[1].chomp
 message = FileManager.new(input, output)
-p message.output_message_text_in
-message.write_braille_to_file
+p message.output_message_braille_in
+message.write_text_to_file
