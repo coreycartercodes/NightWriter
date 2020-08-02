ff891e772f61f80223b19d27df540e4096bc5e97
Corey Carter <crc2231@gmail.com>
2020-08-02 01:23:59 -0600

    Rename 'textfiles' directory to 'data'

--- test/text_to_braille_test.rb
+++ test/text_to_braille_test.rb
@@ -3,19 +3,26 @@ require "./lib/text_to_braille"
 
 class TextToBrailleTest < Minitest::Test
 
+  def setup
+    @dictionary = TextToBraille.new
+  end
+
   def test_it_exists
-    dictionary = TextToBraille.new
-    assert_instance_of TextToBraille, dictionary
+    assert_instance_of TextToBraille, @dictionary
   end
 
   def test_it_calls
-    dictionary = TextToBraille.new
-    assert_equal {top: "0.", middle: "..", bottom: ".."}, dictionary.text_to_braille["a"]
-    assert_equal {top: "..", middle: "00", bottom: "0."}, dictionary.text_to_braille["!"]
+    expected1 = {top: "0.", middle: "..", bottom: ".."}
+    expected2 = {top: "..", middle: "00", bottom: "0."}
+    assert_equal expected1, @dictionary.text_to_braille["a"]
+    assert_equal expected2, @dictionary.text_to_braille["!"]
   end
 
+  def test_it_translates_letters
+    skip
+   @dictionary.alpha_to_braille("h")
+    assert_equal "0.", top
+  end
+
+
 end
-
-dictionary = TextToBraille.new
-
-dictionary.alpha_to_braille("h")
