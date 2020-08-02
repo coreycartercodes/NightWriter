2c9398b7c287c2d56f136da827f9505d3f5b031e
Corey Carter <crc2231@gmail.com>
2020-08-01 22:04:22 -0600

    Add create_lines test

--- test/file_manager_test.rb
+++ test/file_manager_test.rb
@@ -19,12 +19,19 @@ class FileManagerTest < Minitest::Test
   end
 
   def test_output_message
-    assert_equal "Created 'braille.txt' containing 593 characters", @message.output_message
+    assert_equal "Created 'braille.txt' containing 590 characters", @message.output_message
   end
 
   def test_output_file_exists
     @message.output_file
     assert File.exists?('./textfiles/braille.txt')
   end
+
+  def test_create_lines
+   assert_equal 40, @message.create_lines(0).length
+   assert_equal 40, @message.create_lines(2).length
+  end
+
+  # p @message.limit_characters
 
 end
