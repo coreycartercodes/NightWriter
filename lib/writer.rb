ccf55e24b8c5c5a9db313b073c51ba6d33acfb43
Corey Carter <crc2231@gmail.com>
2020-08-02 01:21:13 -0600

    Add parts of lines method and test

--- lib/writer.rb
+++ lib/writer.rb
@@ -1,12 +1,37 @@
 require './lib/text_to_braille'
 
 class Writer
-  attr_reader :alpha,
+  attr_reader
 
   def initialize
     @alpha = TextToBraille.new
   end
 
-  
-
+  def alpha_to_braille_top(line)
+    top_line = ""
+    line.each do |character|
+      top_line << @alpha.alpha_to_braille_top(character)
+    end
+    top_line
   end
+
+  def alpha_to_braille_middle(line)
+    middle_line = ""
+    line.each do |character|
+      middle_line << @alpha.alpha_to_braille_middle(character)
+    end
+    middle_line
+  end
+
+  def alpha_to_braille_bottom(line)
+    bottom_line = ""
+    line.each do |character|
+      bottom_line << @alpha.alpha_to_braille_bottom(character)
+    end
+    bottom_line
+  end
+
+
+
+
+end
--- test/writer_test.rb
+++ test/writer_test.rb
@@ -11,4 +11,12 @@ class WriterTest < Minitest::Test
     assert_instance_of Writer, @writer
   end
 
+  def test_it_can_write_parts_of_lines
+    line = ["m", "o", "m"]
+
+    assert_equal "000.00", @writer.alpha_to_braille_top(line)
+    assert_equal "...0..", @writer.alpha_to_braille_middle(line)
+    assert_equal "0.0.0.", @writer.alpha_to_braille_bottom(line)
+  end
+
 end
